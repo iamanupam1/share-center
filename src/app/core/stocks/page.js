7959f6fdbedd4c32e7e39d604ad02f431347b3cd
iamanupam1 <anupamdhakal20@gmail.com>
2023-10-08 18:23:18 +0545

    quote change

--- src/app/core/stocks/page.js
+++ src/app/core/stocks/page.js
@@ -19,7 +19,7 @@ function StockPage() {
         <Table.Body className="divide-y">
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Apple MacBook Pro 17"
+              Apple MacBook Pro 17
             </Table.Cell>
             <Table.Cell>Sliver</Table.Cell>
             <Table.Cell>Laptop</Table.Cell>
